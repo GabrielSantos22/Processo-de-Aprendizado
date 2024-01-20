06bf43671b7fd318faa11a0b5a8219f5ddeb4efc
GabrielGSantos <bielsantos623@gmail.com>
2024-01-20 10:32:14 -0300

    Organização do roteiro de estudo

---
+++ src/Estrutura.java
@@ -0,0 +1,20 @@
+public class Estrutura {
+    public static void main(String[] args) {
+
+        String segunda = "Conhecimentos Bancários e Informática";
+        System.out.println(segunda);
+
+        String terca = "Matemática Financeira e Probabilidade e Estatística";
+        System.out.println(terca);
+
+        String quarta = "Atendimento Bancário e Português";
+        System.out.println(quarta);
+
+        String quinta = "Conhecimentos Bancários e Informática";
+        System.out.println(quinta);
+
+        String sexta = "Redação e Português";
+        System.out.println(sexta);
+
+    }
+}
